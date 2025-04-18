74ab6d8d76d57717288705e7c60c9a36833cc72f
ghnn-n <cgh6470@gmail.com>
2025-04-18 12:35:02 +0900

    [Design] #3 - 테이블 뷰 추가

---
+++ PhoneBook/TableViewCell.swift
@@ -0,0 +1,72 @@
+//
+//  TableViewCell.swift
+//  PhoneBook
+//
+//  Created by 최규현 on 4/18/25.
+//
+
+import UIKit
+import SnapKit
+
+class TableViewCell: UITableViewCell {
+    
+    static let id = "TableViewCell"
+    
+    let image: UIImageView = {
+        let image = UIImageView()
+        image.contentMode = .scaleAspectFit
+        image.backgroundColor = .white
+        image.layer.borderWidth = 3
+        image.layer.borderColor = CGColor(red: 0.7, green: 0.7, blue: 0.7, alpha: 1)
+        image.layer.cornerRadius = 30
+        
+        return image
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 16)
+        label.textColor = .black
+        
+        return label
+    }()
+    
+    let numberLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 16)
+        label.textColor = .black
+        
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupUI()
+    }
+    
+    private func setupUI() {
+        [image, nameLabel, numberLabel].forEach { contentView.addSubview($0) }
+        
+        image.snp.makeConstraints {
+            $0.width.height.equalTo(60)
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().inset(20)
+        }
+        
+        nameLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalTo(image.snp.trailing).offset(10)
+        }
+        
+        numberLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(40)
+        }
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- PhoneBook/ViewController.swift
+++ PhoneBook/ViewController.swift
@@ -10,7 +10,7 @@ import SnapKit
 
 class ViewController: UIViewController {
     
-    let titleLabel: UILabel = {
+    private let titleLabel: UILabel = {
         let label = UILabel()
         label.text = "친구 목록"
         label.textAlignment = .center
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
         return label
     }()
     
-    lazy var addButton: UIButton = {
+    private lazy var addButton: UIButton = {
         let button = UIButton()
         button.setTitle("추가", for: .normal)
         button.setTitleColor(.gray, for: .normal)
@@ -27,6 +27,15 @@ class ViewController: UIViewController {
         button.addTarget(self, action: #selector(addButtonTapped), for: .touchUpInside)
         
         return button
+    }()
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(TableViewCell.self, forCellReuseIdentifier: TableViewCell.id)
+        
+        return tableView
     }()
     
     override func viewDidLoad() {
@@ -37,7 +46,7 @@ class ViewController: UIViewController {
     }
     
     func setupUI() {
-        [titleLabel, addButton].forEach { view.addSubview($0) }
+        [titleLabel, addButton, tableView].forEach { view.addSubview($0) }
         
         titleLabel.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide).inset(20)
@@ -49,11 +58,39 @@ class ViewController: UIViewController {
             $0.trailing.equalTo(view.safeAreaLayoutGuide).inset(20)
             $0.height.equalTo(titleLabel.snp.height)
         }
+        
+        tableView.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(20)
+            $0.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
     }
     
     @objc func addButtonTapped(_ sender: UIButton) {
         
     }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        80
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TableViewCell.id, for: indexPath) as? TableViewCell else { return UITableViewCell() }
+        
+        cell.nameLabel.text = "name"
+        cell.numberLabel.text = "010-0000-0000"
+        
+        return cell
+    }
+    
+    
 }
 
 #Preview{ ViewController() }
